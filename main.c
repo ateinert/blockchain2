59d9769fead26f95e37197d4214e650b615e7341
ateinert <44173880+ateinert@users.noreply.github.com>
2018-12-03 12:51:09 -0600

    Update main.c

--- main.c
+++ main.c
@@ -63,7 +63,7 @@ int main(int argc, char **argv)
 			}
 			Block block = createBlock(blockcount, buffer);
 			//fprintf(stderr, "Block: %s", block.blockTitle);
-			int s, i
+			int s, i;
 			s = connectTCP(host, service);
 			broadcastBlock(block, hosts, s, numHosts);
 			/*for (i = 0; i < 1; i++)
